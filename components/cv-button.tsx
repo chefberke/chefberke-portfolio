677e97cc23b7274bf7dfb5c650deb4504b9e96f0
Berke Kanber <115804625+chefberke@users.noreply.github.com>
2025-06-11 22:05:10 +0300

    Update cv-button.tsx

--- components/cv-button.tsx
+++ components/cv-button.tsx
@@ -5,7 +5,7 @@ function CvButton() {
   return (
     <div className="bg-neutral-800 rounded-md w-fit px-2.5 py-1 hover:opacity-85 transition-all">
       <a
-        href="/berkekanber-resume.pdf"
+        href="/Berke_Kanber_2025_CV.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="font-normal text-sm flex items-center gap-2"
